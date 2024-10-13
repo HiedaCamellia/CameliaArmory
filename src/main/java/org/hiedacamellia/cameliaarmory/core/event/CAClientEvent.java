886b6bd840021cb4a024f0e75c0891279ec3aad8
Yuqi154 <1123489751@qq.com>
2024-10-13 17:32:10 +0800

    0.0.1

--- src/main/java/org/hiedacamellia/cameliaarmory/core/event/CAClientEvent.java
+++ src/main/java/org/hiedacamellia/cameliaarmory/core/event/CAClientEvent.java
@@ -7,7 +7,6 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import org.hiedacamellia.cameliaarmory.CameliaArmory;
 import org.hiedacamellia.cameliaarmory.registries.CAEntity;
-import org.hiedacamellia.camellialib.client.render.ThrowableItemRender;
 
 @EventBusSubscriber(modid = CameliaArmory.MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class CAClientEvent {
