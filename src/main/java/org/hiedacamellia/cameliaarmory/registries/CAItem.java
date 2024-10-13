d3a6d0ec3d0e567e0c90ff148993eeff27c84424
Yuqi154 <1123489751@qq.com>
2024-10-13 13:22:32 +0800

    不知道写了什么

--- src/main/java/org/hiedacamellia/cameliaarmory/registries/CAItem.java
+++ src/main/java/org/hiedacamellia/cameliaarmory/registries/CAItem.java
@@ -81,9 +81,9 @@ public class CAItem {
     public static DeferredItem<SwordItemWithTooltip> MACUAHUITL = ITEMS.register("macuahuitl", () -> new SwordItemWithTooltip(Tiers.WOOD,new  Item.Properties().attributes(interactionRange(0.5)).attributes(SwordItem.createAttributes(Tiers.IRON, 6.5F, -2.4F)).stacksTo(1).durability(250)));
 
     public static DeferredItem<Item> ENGLAND_LONGBOW = SPECIAL.registerItem("england_longbow", (i) -> new BowItemWithTootip(
-            new  Item.Properties().attributes(interactionRange(0.5)).stacksTo(1).durability(384)));
+            new  Item.Properties().attributes(attackSpeed(-0.8)).attributes(interactionRange(0.5)).stacksTo(1).durability(384)));
     public static DeferredItem<Item> YUMI = SPECIAL.registerItem("yumi", (i) -> new BowItemWithTootip(
-            new  Item.Properties().attributes(interactionRange(0.5)).stacksTo(1).durability(384)));
+            new  Item.Properties().attributes(attackSpeed(-1)).attributes(interactionRange(0.5)).stacksTo(1).durability(384)));
 
     public static void registry(IEventBus modEventBug) {
         ITEMS.register(modEventBug);
@@ -129,6 +129,10 @@ public class CAItem {
 
     public static ItemAttributeModifiers interactionRange(double amount){
         return ItemAttributeModifiers.builder().add(Attributes.BLOCK_INTERACTION_RANGE, new AttributeModifier(ResourceLocation.fromNamespaceAndPath(CameliaArmory.MODID, "block_interaction_range"),amount, AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND).build();
+    }
+
+    public static ItemAttributeModifiers attackSpeed(double amount){
+        return ItemAttributeModifiers.builder().add(Attributes.ATTACK_SPEED, new AttributeModifier(ResourceLocation.fromNamespaceAndPath(CameliaArmory.MODID, "attack_speed"),amount, AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.HAND).build();
     }
 
 }
